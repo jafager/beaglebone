812d1297f0d80ecc102b01005139bcb4c7f1044d
Jason A. Fager <jafager@jafager.com>
2015-10-17 09:46:08 -0400

    Purged original commented-out code; works

--- dttb_uart/uart.s
+++ dttb_uart/uart.s
@@ -69,89 +69,17 @@ _start:
 
     /* setup_clocks_for_console */
 	bl enable_uart_clocks
-	/*
-    ldr r0, =CM_WKUP_CLKSTCTRL
-    ldr r1, [r0]
-    and r1, #~0x3
-    orr r1, #0x2
-    str r1, [r0]
-    ldr r0, =CM_PER_L4HS_CLKSTCTRL
-    ldr r1, [r0]
-    and r1, #~0x3
-    orr r1, #0x2
-    str r1, [r0]
-    ldr r0, =CM_WKUP_UART0_CLKCTRL
-    ldr r1, [r0]
-    and r1, #~0x3
-    orr r1, #0x2
-    str r1, [r0]
-    ldr r0, =CM_PER_UART0_CLKCTRL
-    ldr r1, [r0]
-    and r1, #~0x3
-    orr r1, #0x2
-    str r1, [r0]
-	*/
 
     /* UART soft reset */
 	bl uart_soft_reset
-	/*
-    ldr r0, =UART0_SYSC
-    ldr r1, [r0]
-    orr r1, #0x2
-    str r1, [r0]
-    ldr r0, =UART0_SYSS
-.uart_soft_reset:
-    ldr r1, [r0]
-    ands r1, #0x1
-    beq .uart_soft_reset
-	*/
 
     /* turn off smart idle */
 	ldr r0, =UART0_BASE
 	bl uart_disable_smart_idle
-	/*
-    ldr r0, =UART0_SYSC
-    ldr r1, [r0]
-    orr r1, #(0x1 << 0x3)
-    str r1, [r0]
-	*/
 
     /* initialize UART */
 	ldr r0, =UART0_BASE
 	bl uart_init
-	/*
-    ldr r0, =UART0_BASE
-    ldr r1, =26
-.uart_init:
-    ldrb    r3, [r0, #20]
-    uxtb    r3, r3
-    tst     r3, #0x40
-    beq     .uart_init
-    mov     r3, #0
-    strb    r3, [r0, #4]
-    mov     r3, #7
-    strb    r3, [r0, #32]
-    mvn     r3, #0x7c
-    strb    r3, [r0, #12]
-    mov     r3, #0
-    strb    r3, [r0]
-    strb    r3, [r0, #4]
-    mov     r3, #3
-    strb    r3, [r0, #12]
-    strb    r3, [r0, #16]
-    mov     r3, #7
-    strb    r3, [r0, #8]
-    mvn     r3, #0x7c
-    strb    r3, [r0, #12]
-    uxtb    r3, r1
-    strb    r3, [r0]
-    ubfx    r1, r1, #8, #8
-    strb    r1, [r0, #4]
-    mov     r3, #3
-    strb    r3, [r0, #12]
-    mov     r3, #0
-    strb    r3, [r0, #32]
-	*/
 
     /* turn on second led */
     ldr r2, =GPIO1_SETDATAOUT
@@ -172,15 +100,6 @@ _start:
     mov     r0, r3
     add     r0, #1
     b .loop
-
-.uart_putc:
-    ldrb    r2, [r1, #20]
-    uxtb    r2, r2
-    tst     r2, #32
-    beq     .uart_putc
-    strb    r0, [r1]
-    bx      lr
-
 
 
 .global uart_putc
