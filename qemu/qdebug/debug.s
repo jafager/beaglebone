e3500bacd964faeb88ff080cce421f9ad315745f
Jason A. Fager <jafager@jafager.com>
2015-10-31 05:55:37 -0400

    Initial import; hex print works

---
+++ qemu/qdebug/debug.s
@@ -0,0 +1,37 @@
+.syntax unified
+
+
+.text
+
+
+/* debug_print_hex: print the contents of r0 in hexadecimal on the console */
+
+.global debug_print_hex
+debug_print_hex:
+
+	value	.req r0
+	width	.req r1
+	digit	.req r2
+
+	mov width, 32
+
+dph_next_digit:
+
+	push {lr}
+
+	sub width, width, 4
+	mov digit, value, lsr width
+	and digit, digit, 0xf
+	add digit, digit, '0'
+	cmp digit, '9'
+	addhi digit, digit, 39
+
+	push {r0-r2}
+	mov r0, digit
+	bl console_putc
+	pop {r0-r2}
+
+	cmp width, 0
+	bne dph_next_digit
+
+	pop {pc}
---
+++ qemu/qdebug/qdebug.s
@@ -0,0 +1,72 @@
+.syntax unified
+
+
+.text
+
+
+.global qdebug
+qdebug:
+
+	ldr sp, =stack_supervisor
+
+	bl console_init
+
+	ldr r0, =message_ready
+	bl console_puts
+
+
+	ldr r0, =0x01234567
+	bl debug_print_hex
+	ldr r0, =crlf
+	bl console_puts
+
+	ldr r0, =0x89abcdef
+	bl debug_print_hex
+	ldr r0, =crlf
+	bl console_puts
+
+	ldr r0, =0xdeadbeef
+	bl debug_print_hex
+	ldr r0, =crlf
+	bl console_puts
+
+	ldr r0, =0xfeedabba
+	bl debug_print_hex
+	ldr r0, =crlf
+	bl console_puts
+
+	ldr r0, =0x867c5309
+	bl debug_print_hex
+	ldr r0, =crlf
+	bl console_puts
+
+	ldr r0, =0x00000000
+	bl debug_print_hex
+	ldr r0, =crlf
+	bl console_puts
+
+	ldr r0, =0xffffffff
+	bl debug_print_hex
+	ldr r0, =crlf
+	bl console_puts
+
+	ldr r0, =0x43218765
+	bl debug_print_hex
+	ldr r0, =crlf
+	bl console_puts
+
+hang:
+
+	b hang
+
+
+/* Messages */
+
+
+message_ready:
+
+	.asciz "Ready.\r\n"
+
+crlf:
+
+	.asciz "\r\n"
