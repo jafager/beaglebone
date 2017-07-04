c0e5dd1375183fce08b9b874c8a06f19c3797679
Jason Fager <jfager@hosting.com>
2017-07-04 09:18:04 -0400

    not working, but progress

--- boot2/boot.c
+++ boot2/boot.c
@@ -5,16 +5,23 @@
 
 
 
+
 #define CM_PER                                      0x44e00000
 #define CM_PER_MMC0_CLKCTRL                         0x3c
 #define CM_PER_MMC0_CLKCTRL_MODULEMODE              (0b11 << 0)
 #define CM_PER_MMC0_CLKCTRL_MODULEMODE_ENABLE       (0b10 << 0)
 #define MMC0                                        0x48060000
+#define SD_ARG                                      0x208
 #define SD_CAPA                                     0x240
 #define SD_CAPA_VS                                  (0b111 << 24)
 #define SD_CAPA_VS_18                               (1 << 26)
 #define SD_CAPA_VS_30                               (1 << 25)
 #define SD_CMD                                      0x20c
+#define SD_CMD_CCCE                                 (1 << 19)
+#define SD_CMD_CICE                                 (1 << 20)
+#define SD_CMD_INDX                                 24
+#define SD_CMD_MASK                                 0x3ffb0037
+#define SD_CMD_RSP_TYPE_48                          (0x2 << 16)
 #define SD_CON                                      0x12c
 #define SD_CON_DW8                                  (1 << 5)
 #define SD_CON_DW8_1OR4BIT                          (0 << 5)
@@ -32,6 +39,7 @@
 #define SD_STAT                                     0x230
 #define SD_STAT_CC                                  (1 << 0)
 #define SD_STAT_CLEAR_ALL                           0x337f07ff
+#define SD_STAT_CTO                                 (1 << 16)
 #define SD_STAT_ERRI                                (1 << 15)
 #define SD_SYSCONFIG                                0x110
 #define SD_SYSCONFIG_AUTOIDLE                       (1 << 0)
@@ -49,6 +57,7 @@
 #define SD_SYSCTL_CLKD_1023                         (0x3ff << 6)
 #define SD_SYSCTL_ICE                               (1 << 0)
 #define SD_SYSCTL_ICS                               (1 << 1)
+#define SD_SYSCTL_SRC                               (1 << 25)
 #define SD_SYSSTATUS                                0x114
 #define SD_SYSSTATUS_RESETDONE                      (1 << 0)
 
@@ -104,11 +113,20 @@ void boot(void)
     regmask32(MMC0 + SD_IE, SD_IE_ENABLE_ALL, SD_IE_ENABLE_ALL);
     console_puts("Internal interrupts enabled.\r\n");
 
+
+
+    /* This seems like the place where host controller  */
+    /* configuration ends and card configuration begins */
+    console_puts("Press a key to continue...\r\n");
+    console_getc();
+
+
+
     /* Send initialization stream */
     regmask32(MMC0 + SD_CON, SD_CON_INIT, SD_CON_INIT_ENABLE);
     console_puts("Sending initialization stream.\r\n");
 
-    /* Send CMD0 */
+    /* Send CMD0 (I don't think this one goes to the card) */
     regwrite32(MMC0 + SD_CMD, 0);
     console_puts("CMD0 sent.\r\n");
 
@@ -139,4 +157,83 @@ void boot(void)
     /* Clear all internal interrupts */
     regmask32(MMC0 + SD_STAT, SD_STAT_CLEAR_ALL, SD_STAT_CLEAR_ALL);
     console_puts("Internal interrupts cleared.\r\n");
+
+    /* TODO: Change clock frequency to fit protocol */
+
+    /* Send CMD0 (this one goes to the card) */
+    regwrite32(MMC0 + SD_CMD, 0);
+    console_puts("CMD0 sent.\r\n");
+
+    /* Wait for CMD0 to complete */
+    do
+    {
+        status = regread32(MMC0 + SD_STAT);
+    }
+    while (!((status & SD_STAT_CC) || (status & SD_STAT_ERRI)));
+    if (status & SD_STAT_ERRI)
+    {
+        console_puts("Error in CMD0: 0x");
+        console_hexprint(status);
+        console_puts("\r\n");
+        while (1);
+    }
+    console_puts("CMD0 complete.\r\n");
+
+    /* Clear CC internal interrupt */
+    regmask32(MMC0 + SD_STAT, SD_STAT_CC, SD_STAT_CC);
+    console_puts("CC interrupt cleared.\r\n");
+
+    /* Send CMD5 */
+    regmask32(MMC0 + SD_CMD, SD_CMD_MASK, (5 << SD_CMD_INDX) | SD_CMD_CICE | SD_CMD_CCCE | SD_CMD_RSP_TYPE_48);
+    console_puts("CMD5 sent.\r\n");
+
+    /* Wait for CMD5 to complete */
+    do
+    {
+        status = regread32(MMC0 + SD_STAT);
+    }
+    while (!((status & SD_STAT_CC) || (status & SD_STAT_ERRI)));
+    if (status & SD_STAT_CC)
+    {
+        console_puts("Card is SDIO.\r\n");
+        console_puts("SDIO cards are not supported.\r\n");
+        while (1);
+    }
+    if (!(status & SD_STAT_CTO))
+    {
+        console_puts("Error in CMD5: 0x");
+        console_hexprint(status);
+        console_puts("\r\n");
+        while (1);
+    }
+    console_puts("CMD5 timed out.\r\n");
+    console_puts("Card is not SDIO.\r\n");
+
+    /* Software reset for mmc_cmd line (yes this is weird but it follows the TRM) */
+    regmask32(MMC0 + SD_SYSCTL, SD_SYSCTL_SRC, SD_SYSCTL_SRC);
+    while (!(regread32(MMC0 + SD_SYSCTL) & SD_SYSCTL_SRC));
+    while (regread32(MMC0 + SD_SYSCTL) & SD_SYSCTL_SRC);
+
+    /* Send CMD8 */
+    regwrite32(MMC0 + SD_ARG, 0xaa);
+    regmask32(MMC0 + SD_CMD, SD_CMD_MASK, (8 << SD_CMD_INDX) | SD_CMD_CICE | SD_CMD_CCCE | SD_CMD_RSP_TYPE_48);
+    console_puts("CMD8 sent.\r\n");
+
+    /* Wait for CMD8 to complete */
+    do
+    {
+        status = regread32(MMC0 + SD_STAT);
+    }
+    while (!((status & SD_STAT_CC) || (status & SD_STAT_ERRI)));
+    if ((status & SD_STAT_ERRI) && !(status & SD_STAT_CTO))
+    {
+        console_puts("Error in CMD8: 0x");
+        console_hexprint(status);
+        console_puts("\r\n");
+        while (1);
+    }
+    if (status & SD_STAT_CC)
+        console_puts("Card supports 2.0 specification.\r\n");
+    else
+        console_puts("Card does not support 2.0 specification.\r\n");
 }
