17a88f61c2b8621700990d7656be4c82f6f1ddda
Jason Fager <jfager@hosting.com>
2017-07-10 18:23:47 -0400

    Working, more or less final

--- ramtest2/ramtest.c
+++ ramtest2/ramtest.c
@@ -20,6 +20,7 @@ void ramtest(void)
 
     console_puts("\r\n");
 
+    uint32_t test_count = 0;
     while (1)
     {
         console_puts("Beginning write test...\r\n");
@@ -30,16 +31,14 @@ void ramtest(void)
                 for (uint32_t offset = 0; offset < (128 * 1024); offset++)
                 {
                     uint32_t *address = 0x80000000 + (row * 64 * 128 * 1024 * 4) + (column * 128 * 1024 * 4) + (offset * 4);
-                    *address = 0xdeadbeef;
+                    *address = 0xdeadbeef + ((uint32_t)address & 0xffff) + ((test_count & 0xffff) << 18);
                 }
                 console_puts(".");
             }
             console_puts("\r\n");
         }
         console_puts("Write test complete.\r\n");
-
         console_puts("\r\n");
-
         console_puts("Beginning read test...\r\n");
         for (uint32_t row = 0; row < 16; row++)
         {
@@ -49,12 +48,15 @@ void ramtest(void)
                 {
                     uint32_t *address = 0x80000000 + (row * 64 * 128 * 1024 * 4) + (column * 128 * 1024 * 4) + (offset * 4);
                     uint32_t value = *address;
-                    if (value != 0xdeadbeef)
+                    uint32_t expected_value = 0xdeadbeef + ((uint32_t)address & 0xffff) + ((test_count & 0xffff) << 18);
+                    if (value != expected_value)
                     {
                         console_puts("\r\n");
-                        console_puts("Read test failed at address ");
-                        console_hexprint(address);
-                        console_puts(", expected value 0xdeadbeef, actual value ");
+                        console_puts("Read test failed at address 0x");
+                        console_hexprint((uint32_t)address);
+                        console_puts(", expected value 0x");
+                        console_hexprint(expected_value);
+                        console_puts(", actual value 0x");
                         console_hexprint(value);
                         console_puts("\r\n");
                         while (1);
@@ -65,5 +67,7 @@ void ramtest(void)
             console_puts("\r\n");
         }
         console_puts("Read test complete.\r\n");
+        console_puts("\r\n");
+        test_count++;
     }
 }
